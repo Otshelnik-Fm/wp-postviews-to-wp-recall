ca36db668b83de1bb8b0d87932ecb044f75b9463
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2019-04-17 20:30:16 +0400

    v1.1

--- index.php
+++ index.php
@@ -1,164 +1,195 @@
 <?php
+
 /*
-    Интеграция WP-Recall с плагином "WP-PostViews"
-    https://wordpress.org/plugins/wp-postviews/
-*/
+  Интеграция WP-Recall с плагином "WP-PostViews"
+  https://wordpress.org/plugins/wp-postviews/
+ *
+ *
+  ╔═╗╔╦╗╔═╗╔╦╗
+  ║ ║ ║ ╠╣ ║║║ https://otshelnik-fm.ru
+  ╚═╝ ╩ ╚  ╩ ╩
 
+ */
 
-// проверка что не активирован WP-PostViews или доп публикаций
-function wppv_wpr_check_activate(){
-    if (!function_exists('the_views') || !function_exists('rcl_add_postlist_posts')){
+if ( ! defined( 'ABSPATH' ) )
+    exit;
+
+/* проверка что не активирован WP-PostViews или доп публикаций */
+function wppv_wpr_check_activate() {
+    if ( ! function_exists( 'the_views' ) || ! function_exists( 'rcl_get_postslist' ) ) {
         return false;
     }
 }
 
-
-
 // добавляем в постлист - шаблон posts-list.php
-function wppv_wpr_add_in_to_postlist($content){
+add_filter( 'content_postslist', 'wppv_wpr_add_in_to_postlist' );
+function wppv_wpr_add_in_to_postlist( $content ) {
 
-    if(wppv_wpr_check_activate() === false) return $content; // не активен WP-PostViews или доп публикаций
+    if ( wppv_wpr_check_activate() === false )
+        return $content; // не активен WP-PostViews или доп публикаций
 
     global $post;
 
-    $cnt = intval(get_post_meta($post->ID, 'views', true)); // получаем целое значение
-    if($cnt){
-        $content .= '<span class="wppv_wpr_total" title="Всего просмотров">';
-            $content .= '<i class="fa fa-eye"></i>'.$cnt;
+    $cnt = intval( get_post_meta( $post->ID, 'views', true ) ); // получаем целое значение
+    if ( $cnt ) {
+        $content .= '<span class="pv_wpr_total" title="Всего просмотров">';
+        $content .= '<i class="rcli fa-eye"></i>';
+        $content .= '<span>' . $cnt . '</span>';
         $content .= '</span>';
     }
     return $content;
 }
-add_filter('content_postslist','wppv_wpr_add_in_to_postlist');
-
-
 
 // инлайн стили. Функция сама очистит от пробелов, переносов и сожмёт в строку
-function wppv_wpr_inline_styles($styles){
+add_filter( 'rcl_inline_styles', 'wppv_wpr_inline_styles', 10 );
+function wppv_wpr_inline_styles( $styles ) {
 
-    if(!rcl_is_office()) return $styles; // отработает только в ЛК
+    if ( ! rcl_is_office() )
+        return $styles; // отработает только в ЛК
 
-    if(wppv_wpr_check_activate() === false) return $styles; // не активен WP-PostViews или доп публикаций - не выводим стили ниже
+    if ( wppv_wpr_check_activate() === false )
+        return $styles; // не активен WP-PostViews или доп публикаций - не выводим стили ниже
 
     $styles .= '
-        .wppv_wpr_total {
-            background-color: rgba(219, 219, 219, 0.6);
-            float: right;
-            font: 12px/1 Helvetica,serif,arial;
-            margin: 0 4px;
-            padding: 5px;
+        #pv_wpr_table a:hover {
+            text-decoration: underline;
+        }
+        .pv_wpr_total {
+            align-items: center;
+            display: flex;
             white-space: nowrap;
         }
-        .wppv_wpr_total .fa {
-            margin: 0 5px 0 0;
+        .pv_wpr_total .rcli {
+            color: #2eae5c;
+            font-size: 1em;
+            margin: 0 6px 0 0;
         }
-        #subtab-wppv_top {
-            box-shadow: 0 0 1px 1px #ddd;
+        .pv_wpr_type {
+            align-self: center;
+            color: #999;
         }
-        .wppv_line {
-            color: #777;
-            display: inline-block;
-            padding: 5px 0;
-            width: 100%;
+        .rcl_author_postlist .rcl-table__row:not(.rcl-table__row-header) > div:nth-child(2) a {
+            flex-grow: 1;
         }
-        .wppv_line:nth-child(2n) {
-            background-color: rgba(237, 237, 237, 0.8);
-        }
-        .wppv_type {
-            display: inline-block;
-            padding: 0 0 0 5px;
-            text-align: center;
-            width: 28px;
-        }
-        .wppv_date {
-            font-size: 12px;padding: 0 10px 0 5px;
+        .rcl_author_postlist .rcl-table__row:not(.rcl-table__row-header) > div:nth-child(2) .rating-rcl {
+            order: 1;
         }
     ';
+
     return $styles;
 }
-add_filter('rcl_inline_styles','wppv_wpr_inline_styles',10);
-
-
-
 
 // добавим в вкладку "Публикации" дочернюю "Самые просматриваемые"
-function wppv_wpr_add_second_sub_tab(){
+add_action( 'rcl_setup_tabs', 'wppv_wpr_add_second_sub_tab', 11 );
+function wppv_wpr_add_second_sub_tab() {
 
-    if(wppv_wpr_check_activate() === false) return false; // не активен WP-PostViews или доп публикаций - не выводим вкладку
+    if ( wppv_wpr_check_activate() === false )
+        return false; // не активен WP-PostViews или доп публикаций - не выводим вкладку
 
     $subtab = array(
-        'id'=> 'wppv_top',
-        'name'=> 'Самые просматриваемые',
-        'icon' => 'fa-eye',
-        'callback'=>array(
-            'name'=>'wppv_wpr_top_views',
+        'id'       => 'wppv_top',
+        'name'     => 'Самые просматриваемые',
+        'icon'     => 'fa-eye',
+        'callback' => array(
+            'name' => 'wppv_wpr_top_views',
         )
     );
-    rcl_add_sub_tab('publics',$subtab);
+    rcl_add_sub_tab( 'publics', $subtab );
 }
-add_action('rcl_setup_tabs','wppv_wpr_add_second_sub_tab',11);
-
 
 // обработчик вкладки "Самые просматриваемые"
-function wppv_wpr_top_views($user_lk){
+function wppv_wpr_top_views( $user_lk ) {
     global $wpdb;
 
     $sql = "SELECT t1.ID, t1.post_date, t1.post_title, t1.post_type, t2.meta_value "
-            . "FROM $wpdb->posts AS t1 "
-            . "INNER JOIN $wpdb->postmeta AS t2 "
-            . "ON t1.ID = t2.post_id "
-            . "WHERE "
-                . "t1.post_author = ".$user_lk." "
-                ."AND t2.meta_key LIKE 'views' "
-                ."AND t1.post_status = 'publish' "
-                ."AND t1.post_type  IN ('post', 'post-group', 'products') "
-            . "ORDER BY "
-                . "t2.meta_value DESC "
-            . "LIMIT 0, 50";
+        . "FROM $wpdb->posts AS t1 "
+        . "INNER JOIN $wpdb->postmeta AS t2 "
+        . "ON t1.ID = t2.post_id "
+        . "WHERE "
+        . "t1.post_author = " . $user_lk . " "
+        . "AND t2.meta_key LIKE 'views' "
+        . "AND t1.post_status = 'publish' "
+        . "AND t1.post_type  IN ('post', 'post-group', 'products') "
+        . "ORDER BY "
+        . "t2.meta_value DESC "
+        . "LIMIT 0, 50";
 
-	$results = $wpdb->get_results($sql, ARRAY_A);
+    $results = $wpdb->get_results( $sql, ARRAY_A );
 
-    return wppv_wpr_html_output($results, $ttl = 'Всего просмотров');
+    return wppv_wpr_html_output( $results, $ttl = 'Всего просмотров' );
 }
 
-
+function wppv_notice_box( $text, $type = 'success' ) {
+    return '<div class="notify-lk"><div class="' . $type . '">' . $text . '</div></div>';
+}
 
 // вывод контента
-function wppv_wpr_html_output($results, $ttl){
-    foreach($results as $result){
-        $name = wppv_wpr_post_type_convert($result['post_type']);
-        $out .= '<div class="wppv_line wppv_top">';
-            $out .= '<span class="wppv_type">'.$name.'</span>';
-            $out .= '<span class="wppv_date">'.mysql2date('Y-m-d', $result['post_date']).'</span>';
-            $out .= '<span class="wppv_title">';
-                $out .= '<a target="_blank" href="'.get_permalink($result['ID']).'">'.$result['post_title'].'</a>';
-            $out .= '</span>';
-            $out .= '<span class="wppv_views wppv_wpr_total" title="'.$ttl.'">';
-                $out .= '<i class="fa fa-eye"></i>'.$result['meta_value'];
-            $out .= '</span>';
-        $out .= '</div>';
+function wppv_wpr_html_output( $results, $ttl ) {
+    if ( ! $results )
+        return wppv_notice_box( 'Пока просмотров нет' );
+
+    $Table = new Rcl_Table( array(
+        'cols'     => array(
+            array(
+                'align' => 'center',
+                'title' => 'Тип',
+                'width' => 5
+            ),
+            array(
+                'align' => 'center',
+                'title' => 'Дата',
+                'width' => 15
+            ),
+            array(
+                'title' => 'Заголовок',
+                'width' => 65
+            ),
+            array(
+                'align' => 'center',
+                'title' => 'Просмотры',
+                'width' => 15
+            )
+        ),
+        'table_id' => 'pv_wpr_table',
+        'zebra'    => true,
+        'class'    => 'pv_wpr_views_table',
+        'border'   => array( 'table', 'cols', 'rows' )
+        ) );
+
+    foreach ( $results as $result ) {
+        $name    = wppv_wpr_post_type_convert( $result['post_type'] );
+        $content = '<a target="_blank" href="' . get_permalink( $result['ID'] ) . '">' . $result['post_title'] . '</a>';
+        $status  = '<span class="pv_wpr_views pv_wpr_total" title="' . $ttl . '">';
+        $status  .= '<i class="rcli fa-eye"></i>';
+        $status  .= '<span>' . $result['meta_value'] . '</span>';
+        $status  .= '</span>';
+
+        $Table->add_row( array(
+            $name,
+            mysql2date( 'Y-m-d', $result['post_date'] ),
+            $content,
+            $status
+        ) );
     }
-    return $out;
+
+    return $Table->get_table();
 }
-
-
 
 // конверт типа записи в иконку
-function wppv_wpr_post_type_convert($type){
-    switch($type){
+function wppv_wpr_post_type_convert( $type ) {
+    switch ( $type ) {
         case 'post':
-            $out = '<i class="fa fa-pencil" title="Публикация"></i>';
+            $out = '<i class="rcli fa-pencil pv_wpr_type" title="Публикация"></i>';
             break;
         case 'post-group':
-            $out = '<i class="fa fa-users" title="Публикация в группе"></i>';
+            $out = '<i class="rcli fa-users pv_wpr_type" title="Публикация в группе"></i>';
             break;
         case 'products':
-            $out = '<i class="fa fa-shopping-cart" title="Публикация в магазине"></i>';
+            $out = '<i class="rcli fa-shopping-cart pv_wpr_type" title="Публикация в магазине"></i>';
             break;
     }
     return $out;
 }
-
 
 // прямые sql запросы для дебага
 /*
---
+++ readme.txt
@@ -0,0 +1,56 @@
+== Установка/Обновление ==
+
+<h3 style="text-align: center;">Установка:</h3>
+Т.к. это дополнение для WordPress плагина <a href="https://codeseller.ru/groups/plagin-wp-recall-lichnyj-kabinet-na-wordpress/" target="_blank">WP-Recall</a>, то оно устанавливается через <a href="https://codeseller.ru/obshhie-svedeniya-o-dopolneniyax-wp-recall/" target="_blank"><strong>менеджер дополнений WP-Recall</strong></a>.
+
+1. В админке вашего сайта перейдите на страницу "WP-Recall" -> "Дополнения" и в самом верху нажмите на кнопку "Обзор", выберите .zip архив дополнения на вашем пк и нажмите кнопку "Установить".
+2. В списке загруженных дополнений, на этой странице, найдите это дополнение, наведите на него курсор мыши и нажмите кнопку "Активировать". Или выберите чекбокс и в выпадающем списке действия выберите "Активировать". Нажмите применить.
+
+
+<h3 style="text-align: center;">Обновление:</h3>
+Дополнение поддерживает <a href="https://codeseller.ru/avtomaticheskie-obnovleniya-dopolnenij-plagina-wp-recall/" target="_blank">автоматическое обновление</a> - два раза в день отправляются вашим сервером запросы на обновление.
+Если в течении суток вы не видите обновления (а на странице дополнения вы видите что версия вышла новая), советую ознакомиться с этой <a href="https://codeseller.ru/post-group/rabota-wordpress-krona-cron-prinuditelnoe-vypolnenie-kron-zadach-dlya-wp-recall/" target="_blank">статьёй</a>
+
+
+
+
+== FAQ ==
+<h3>Есть настройки?</h3>
+- настроек нет.
+
+
+<h3>Не отображаются просмотры. Почему?</h3>
+- проверьте что плагин учета просмотров <a href="https://wordpress.org/plugins/wp-postviews/" target="_blank">WP-PostViews</a> у вас установлен и активирован.
+
+
+<h3>У меня вопрос по плагину WP-PostViews...</h3>
+- все вопросы по плагину задавайте его автору. Дополнение поддерживается только в рамках заявленного функционала. Ну или индивидуально вопросы по плагину - на платной основе.
+
+
+<h3>Возможна интеграция с платным дополнением, плагином?</h3>
+- этот частный вопрос требует индивидуального подхода. Только на платной основе готов рассмотреть возможные варианты. Но только по наличию свободного времени.
+
+
+
+
+== Changelog ==
+= 2019-04-17 =
+v1.1
+* работа с WP-Recall v16.17
+* работа с плагином WP-PostViews версии 1.76
+
+
+= 2017-01-18 =
+v1.0
+* Release
+
+
+
+
+== Прочее ==
+
+* Поддержка осуществляется в рамках текущего функционала дополнения
+* При возникновении проблемы, создайте соотвествующую тему на форуме поддержки товара
+* Если вам нужна доработка под ваши нужды - вы можете обратиться ко мне в <a href="https://codeseller.ru/author/otshelnik-fm/?tab=chat" target="_blank">ЛС</a> с техзаданием на платную доработку.
+
+Все мои работы опубликованы <a href="https://otshelnik-fm.ru/?p=2562&utm_source=free-addons&utm_medium=addon-description&utm_campaign=wp-postviews-to-wp-recall&utm_content=codeseller.ru&utm_term=all-my-addons" target="_blank">на моём сайте</a> и в каталоге магазина <a href="https://codeseller.ru/author/otshelnik-fm/?tab=publics&subtab=type-products" target="_blank">CodeSeller.ru</a>
